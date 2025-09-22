fa3b4def2d091cd4dd0cb8931d596317db9c02ca
Bob Reilly <robert.reilly@peraton.com>
2025-09-22 07:41:26 -0400

    initial commit

---
+++ src/Solution.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        return res;
+
+    }
+}
---
+++ src/SolutionTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @org.junit.jupiter.api.Test
+    void postorderTraversal() {
+    }
+}
